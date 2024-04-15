b4e15ca9e6b4144959d67a4478cc3fffaf8ac319
Darkrager7 <123085155+Darkrager7@users.noreply.github.com>
2024-04-15 17:36:14 +0100

    Comments
    Commenting on various parts of the program to increase understanding and readability.

--- Glowing-Grates-VCAttempt/Main.js
+++ Glowing-Grates-VCAttempt/Main.js
@@ -1,4 +1,5 @@
-
+//Definition of function to enable high contrast mode
+//Program checks if high contrast mode has been enabled, when enabled via button, the stylesheet it's linked to will overwrite the settings on the page
 function toggleHighContrastMode(){
     const isHighContrast = document.documentElement.classList.toggle('high-contrast-mode');
     localStorage.setItem('high-contrast-mode', isHighContrast);
