78fa0113ab1c5ac3e8791c40739a1fda00d050c7
Darkrager7 <123085155+Darkrager7@users.noreply.github.com>
2024-04-15 17:19:40 +0100

    High Contrast Button & Maps
    An addition of a high contrast button next to the navbar, as well as adjusting the positioning and anchoring of the map embed through CSS

--- Glowing-Grates-VCAttempt/Main.js
+++ Glowing-Grates-VCAttempt/Main.js
@@ -1,1 +1,12 @@
-alert("Yo")
+
+function toggleHighContrastMode(){
+    const isHighContrast = document.documentElement.classList.toggle('high-contrast-mode');
+    localStorage.setItem('high-contrast-mode', isHighContrast);
+
+}
+
+const isHighContrast = localStorage.getItem('high-contrast-mode') === 'true';
+
+if(isHighContrast) {
+    document.documentElement.classList.add('high-contrast-mode');
+}
